4a917c12f50ad2adf99c77858b3880352419dcfd
Olli Niskanen <olli@3dbear.fi>
2018-10-18 19:21:53 +0300

    Initial commit

---
+++ Assets/Flip.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flip : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{			
+			transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+			Debug.LogFormat("Flip. New scale: {0}", transform.localScale);
+		}
+	}
+}
